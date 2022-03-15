14916f23e1de1fd25ec09e4f03636946f427ebdc
adelalira <aalira.96@gmail.com>
2022-03-15 11:35:36 +0100

    clases creadas

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,25 +2,13 @@ package com.example.demo.model;
 
 public class User {
 
+	private long id;
+	private String name;
+	private String lastName;
 	private String email;
 	private String password;
-	public User(String email, String password) {
-		super();
-		this.email = email;
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
+	
 	
 	
 	
