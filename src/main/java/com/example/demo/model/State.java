41d14ca13b86783c15ae86fc4707cd1eeddb0a2a
lauramolez <lauramorelez@gmail.com>
2022-03-15 12:32:13 +0100

    modificado modelo - usuario y enumerado

--- src/main/java/com/example/demo/model/State.java
+++ src/main/java/com/example/demo/model/State.java
@@ -1,5 +1,17 @@
 package com.example.demo.model;
 
+/**
+ * Representa los posibles estados que pueden tener las incidencias.
+ * 
+ * @author laura
+ *
+ */
 public enum State {
+
+	RESULTA,
+
+	EN_PROCESO,
+
+	ENVIADA;
 
 }
