efdcf27b3e1e80020a5957fc330f40a1349d4d6d
adelalira <aalira.96@gmail.com>
2022-03-21 14:30:56 +0100

    contact us

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -34,6 +34,21 @@ public class CorsConfig implements WebMvcConfigurer {
 	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers","Authorization")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+				 
+				 
+				 registry.addMapping("/sendMail").allowedOrigins("http://localhost:4200")
+	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+	                        "Access-Control-Request-Headers","Authorization")
+	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+				 
+				 
+				 registry.addMapping("/user/email/{email}").allowedOrigins("http://localhost:4200")
+	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+	                        "Access-Control-Request-Headers","Authorization")
+	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+				 
+				 
+				
 
 		};
 		
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,18 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepo;
+
+@Service
 public class UserService {
+	
+	@Autowired UserRepo userRepo;
+
+	public User findByEmail(String email) {
+		return this.userRepo.findByEmail(email);
+	}
 
 }
