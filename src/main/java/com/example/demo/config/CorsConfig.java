b579c3db3746e5cf5e1fed20478983bad932d7cd
lauramolez <lauramorelez@gmail.com>
2022-03-21 17:37:50 +0100

    cambiada url front con variable

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -8,51 +8,66 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 	
+	static final String urlFront = "http://localhost:4200";
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
+			
 
 				/**
 				 * CORS PARA PODER HACER LOGIN
 				 */
-				registry.addMapping("/auth/login").allowedOrigins("http://localhost:4200")
-                .allowedHeaders("GET", "POST", "DELETE", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                       "Authorization", "Access-Control-Request-Headers")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
-        
+				registry.addMapping("/auth/login").allowedOrigins(urlFront)
+						.allowedHeaders("GET", "POST", "DELETE", "OPTIONS", "PUT", "Content-Type", "X-Requested-With",
+								"accept", "Origin", "Access-Control-Request-Method", "Authorization",
+								"Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
 				/**
 				 * CORS PARA PODER HACER UN REGISTRO DE UN USUARIO
 				 */
-				 registry.addMapping("/auth/register").allowedOrigins("http://localhost:4200")
-	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-	                        "Access-Control-Request-Headers")
-	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
-				 
-				 registry.addMapping("/user").allowedOrigins("http://localhost:4200")
-	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-	                        "Access-Control-Request-Headers","Authorization")
-	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
-				 
-				 
-				 registry.addMapping("/sendMail").allowedOrigins("http://localhost:4200")
-	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-	                        "Access-Control-Request-Headers","Authorization")
-	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
-				 
-				 
-				 registry.addMapping("/user/email/{email}").allowedOrigins("http://localhost:4200")
-	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-	                        "Access-Control-Request-Headers","Authorization")
-	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
-				 
-				 
-				
+				registry.addMapping("/auth/register").allowedOrigins(urlFront)
+						.allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept",
+								"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
+				registry.addMapping("/user").allowedOrigins("http://localhost:4200")
+						.allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept",
+								"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+								"Authorization")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
+				/**
+				 * Enviar email
+				 */
+				registry.addMapping("/sendMail").allowedOrigins(urlFront)
+						.allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept",
+								"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+								"Authorization")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
+			
+				registry.addMapping("/user/email/{email}").allowedOrigins(urlFront)
+						.allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept",
+								"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+								"Authorization")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
+				/**
+				 * Comprobación para el guardián
+				 */
+				registry.addMapping("/login").allowedOrigins(urlFront)
+						.allowedMethods("GET", "POST", "OPTIONS", "PUT")
+						.allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "accept", "Origin",
+								"Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
+			};
 
 		};
-		
-	};
 	}
 
 }
